ea0afdadb1f75756ebd791eac0c5f9bde9aa60a4
creising <creising@gmail.com>
2015-09-26 13:03:41 -0400

    got this working

---
+++ src/main/java/org/urbanbyte/cueserver/server/Server.java
@@ -0,0 +1,27 @@
+package org.urbanbyte.cueserver.server;
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * author: Chris Reising
+ */
+@Path("/message")
+public class Server
+{
+    @GET
+    @Path("/{param}")
+    public Response printMessage(@PathParam("param") String msg) {
+
+        String result = "Restful example : " + msg;
+
+        return Response.status(200).entity(result).build();
+
+    }
+}
